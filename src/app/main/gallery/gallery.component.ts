d44b7e6d4749df3940c05fc53b94eb59f592bdff
William Newman <billycoda@gmail.com>
2019-11-20 10:44:41 -0700

    Gallery looking good

--- src/app/main/gallery/gallery.component.ts
+++ src/app/main/gallery/gallery.component.ts
@@ -8,12 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class GalleryComponent implements OnInit {
   imageArr = [];
   showModal = false;
-  modalImage: object;
   image1 = 'https://www.skullshoppe.com/images/skulls/rl%20ud1.jpg';
   image2 = 'https://cdn.mos.cms.futurecdn.net/u8wSHMmMMXzZuAFBCmcsCK.jpg';
-  image3 = 'https://cdn.shopify.com/s/files/1/0038/3867/3008/products/Skull_Head_Statue_016_600x.jpg?v=1541304069';
-  image4 = 'https://upload.wikimedia.org/wikipedia/en/thumb/6/6c/ScorpionMKXRender.png/220px-ScorpionMKXRender.png';
+  image3 =
+    'https://cdn.shopify.com/s/files/1/0038/3867/3008/products/Skull_Head_Statue_016_600x.jpg?v=1541304069';
+  image4 =
+    'https://upload.wikimedia.org/wikipedia/en/thumb/6/6c/ScorpionMKXRender.png/220px-ScorpionMKXRender.png';
   image5 = 'https://i.ytimg.com/vi/Sg0rXa1WnZc/maxresdefault.jpg';
+  modalImage: object;
 
   constructor() {
     this.pushImages();
@@ -22,26 +24,35 @@ export class GalleryComponent implements OnInit {
     this.pushImages();
     this.pushImages();
     this.pushImages();
- }
-
-  ngOnInit() {
   }
+
+  ngOnInit() {}
 
   pushImages() {
     this.imageArr.push({
-      src: this.image1, alt: 'Skull boi', title: 'I dunno'
+      src: this.image1,
+      alt: 'Skull boi',
+      title: 'I dunno'
     });
     this.imageArr.push({
-      src: this.image2, alt: 'Skull boi', title: 'I dunno'
+      src: this.image2,
+      alt: 'Skull boi',
+      title: 'I dunno'
     });
     this.imageArr.push({
-      src: this.image3, alt: 'Skull boi', title: 'I dunno'
+      src: this.image3,
+      alt: 'Skull boi',
+      title: 'I dunno'
     });
     this.imageArr.push({
-      src: this.image4, alt: 'Skull boi', title: 'GET OVER HERE'
+      src: this.image4,
+      alt: 'Skull boi',
+      title: 'GET OVER HERE'
     });
     this.imageArr.push({
-      src: this.image5, alt: 'Vader', title: 'Darth Vader'
+      src: this.image5,
+      alt: 'Vader',
+      title: 'Darth Vader'
     });
   }
 
@@ -53,5 +64,4 @@ export class GalleryComponent implements OnInit {
   hideModal() {
     this.showModal = false;
   }
-
 }
