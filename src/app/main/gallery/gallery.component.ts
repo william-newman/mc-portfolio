7be308b20086abaa97328518ea5d3d9f5a1df1a8
William Newman <billycoda@gmail.com>
2019-11-20 10:29:05 -0700

    Random save

--- src/app/main/gallery/gallery.component.ts
+++ src/app/main/gallery/gallery.component.ts
@@ -6,10 +6,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gallery.component.css']
 })
 export class GalleryComponent implements OnInit {
+  imageArr = [];
+  showModal = false;
+  modalImage: object;
+  image1 = 'https://www.skullshoppe.com/images/skulls/rl%20ud1.jpg';
+  image2 = 'https://cdn.mos.cms.futurecdn.net/u8wSHMmMMXzZuAFBCmcsCK.jpg';
+  image3 = 'https://cdn.shopify.com/s/files/1/0038/3867/3008/products/Skull_Head_Statue_016_600x.jpg?v=1541304069';
+  image4 = 'https://upload.wikimedia.org/wikipedia/en/thumb/6/6c/ScorpionMKXRender.png/220px-ScorpionMKXRender.png';
+  image5 = 'https://i.ytimg.com/vi/Sg0rXa1WnZc/maxresdefault.jpg';
 
-  constructor() { }
+  constructor() {
+    this.pushImages();
+    this.pushImages();
+    this.pushImages();
+    this.pushImages();
+    this.pushImages();
+    this.pushImages();
+ }
 
   ngOnInit() {
+  }
+
+  pushImages() {
+    this.imageArr.push({
+      src: this.image1, alt: 'Skull boi', title: 'I dunno'
+    });
+    this.imageArr.push({
+      src: this.image2, alt: 'Skull boi', title: 'I dunno'
+    });
+    this.imageArr.push({
+      src: this.image3, alt: 'Skull boi', title: 'I dunno'
+    });
+    this.imageArr.push({
+      src: this.image4, alt: 'Skull boi', title: 'GET OVER HERE'
+    });
+    this.imageArr.push({
+      src: this.image5, alt: 'Vader', title: 'Darth Vader'
+    });
+  }
+
+  fullSizeImage(imgIndex: number) {
+    this.showModal = true;
+    this.modalImage = this.imageArr[imgIndex];
+  }
+
+  hideModal() {
+    this.showModal = false;
   }
 
 }
