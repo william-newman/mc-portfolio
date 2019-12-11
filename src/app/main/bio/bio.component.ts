8e7c04f57b6ac4eec009774664c48b7ddbc82244
William Newman <billycoda@gmail.com>
2019-12-11 15:36:53 -0700

    SCSS impl, some responsiveness in place

--- src/app/main/bio/bio.component.ts
+++ src/app/main/bio/bio.component.ts
@@ -1,16 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-bio',
-  templateUrl: './bio.component.html',
-  styleUrls: ['./bio.component.css']
+  selector: "app-bio",
+  templateUrl: "./bio.component.html",
+  styleUrls: ["./bio.component.scss"]
 })
 export class BioComponent implements OnInit {
   redClass: boolean = false; // Strange attempt to use alternate class names in the bio?
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
+  // StarWars = (function() {
+
+  //   /*
+  //    * Constructor
+  //    */
+  //   function StarWars(args) {
+  //     // Context wrapper
+  //     this.el = $(args.el);
+
+  //     // Audio to play the opening crawl
+  //     this.audio = this.el.find('audio').get(0);
+
+  //     // Start the animation
+  //     this.start = this.el.find('.start');
+
+  //     // The animation wrapper
+  //     this.animation = this.el.find('.animation');
+
+  //     // Remove animation and shows the start screen
+  //     this.reset();
+
+  //     // Start the animation on click
+  //     this.start.bind('click', $.proxy(function() {
+  //       this.start.hide();
+  //       this.audio.play();
+  //       this.el.append(this.animation);
+  //     }, this));
+
+  //     // Reset the animation and shows the start screen
+  //     $(this.audio).bind('ended', $.proxy(function() {
+  //       this.audio.currentTime = 0;
+  //       this.reset();
+  //     }, this));
+  //   }
+
+  //   /*
+  //    * Resets the animation and shows the start screen.
+  //    */
+  //   StarWars.prototype.reset = function() {
+  //     this.start.show();
+  //     this.cloned = this.animation.clone(true);
+  //     this.animation.remove();
+  //     this.animation = this.cloned;
+  //   };
+
+  //   return StarWars;
+  // })();
+
+  // new StarWars({
+  //   el : '.starwars'
+  // });
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { environment } from 'src/environments/environment';
 import { AuthenticateService } from '../services/authenticate.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
   versionNumber: string;
+  hamActive: boolean = false; // Hamburger icon bool
 
   constructor(private auth: AuthenticateService) {
     this.versionNumber = this.auth.appVersion;
    }
 
   ngOnInit() {
+  }
+
+  toggleHamburgerActive() {
+    this.hamActive = !this.hamActive;
   }
 
 }
